2e6f568111477d0704fb5a9720f8af430455edcc
Vibha <vibhaprabhu10@gmail.com>
2020-01-22 12:21:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ModTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 3;
+
+            int result = (a - b * (a / b));
+
+            Console.WriteLine(result);
+        }
+    }
+}
